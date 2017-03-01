7cfac1c56189fc0d57196d11eab399bb815298a3
NetADN <netadn@archi.localdomain>
2017-03-01 15:55:27 +0100

    add PageController and model Page with migration

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -2,9 +2,28 @@
 
 @section('content')
 
- 	<div class="title m-b-md">
-        {{ $data->name }}
-    </div>
-    <p>{{ $data->content }}</p>
+
+     <div class="blog-header">
+        <h1 class="blog-title">{{ $data->name }}</h1>
+        <p class="lead blog-description"></p>
+      </div>
+
+      <div class="row">
+
+        <div class="col-sm-8 blog-main">
+
+          <div class="blog-post">
+            <h2 class="blog-post-title">{{ $data->name }}</h2>
+            <p class="blog-post-meta">{{ $data->created_at }} by <a href="#">Mark</a></p>
+            {!! $data->content !!}
+          </div>
+
+        </div><!-- /.blog-main -->
+
+        @include('elements.sidebar')
+
+      </div><!-- /.row -->
+
+    </div><!-- /.container -->
 	
 @endsection
